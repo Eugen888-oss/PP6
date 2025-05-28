2d3c7e6f4fdf037b192595fe3f7012bd41d38dae
eugen.risling@stud.thga.de <Your Email>
2025-05-28 19:55:48 +0200

    Initial commit

---
+++ solutions/print.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+#print_greeting() {
+#    # TODO: echo "Hello from Bash!"
+#    }
+#
+#    print_vars() {
+#        local name="Bash"
+#            local version=5.1
+#                # TODO: printf "Using %s version %.1f\n" "$name" "$version"
+#                }
+#
+#                print_escape() {
+#                    # TODO: printf "This is a newline:\nThis is a tab:\tDone!\n"
+#                        # TODO: printf "\e[32mGreen text\e[0m and normal text\n"
+#                        }
+#
+## Call your functions:
+#print_greeting
+#print_vars
+#print_escape
